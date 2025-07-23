2d26bf926ac07a6c45093952734f25f11c86f888
cohinteractive <charles@ohinteractive.co.nz>
2025-07-23 20:01:50 +1200

    Update CODEX log

--- lib/src/instructions/instruction_templates.dart
+++ lib/src/instructions/instruction_templates.dart
@@ -1,6 +1,19 @@
 import 'llm_instruction_templates.dart';
+import '../../models/llm_merge_strategy.dart';
 
 /// Wrapper class exposing commonly used instruction blocks.
 class InstructionTemplates {
   static const String merge = mergeInstruction;
+
+  /// Returns merge instructions augmented for the given [strategy].
+  static String forStrategy(MergeStrategy strategy) {
+    switch (strategy) {
+      case MergeStrategy.conservative:
+        return '$merge\nUse a conservative merge strategy.';
+      case MergeStrategy.aggressive:
+        return '$merge\nUse an aggressive merge strategy that overwrites conflicting details.';
+      case MergeStrategy.defaultStrategy:
+        return merge;
+    }
+  }
 }
