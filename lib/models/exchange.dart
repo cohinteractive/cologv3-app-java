92efec80a633f1e335eb364828f43bc4c238b651
cohinteractive <charles@ohinteractive.co.nz>
2025-07-26 12:16:25 +1200

    Log summarization UI addition

--- lib/models/exchange.dart
+++ lib/models/exchange.dart
@@ -3,12 +3,14 @@ class Exchange {
   final String? response;
   final DateTime? promptTimestamp;
   final DateTime? responseTimestamp;
+  String? llmSummary;
 
   Exchange({
     required this.prompt,
     required this.promptTimestamp,
     this.response,
     this.responseTimestamp,
+    this.llmSummary,
   });
 
   /// Returns true if both [prompt] and [response] contain non-empty text.
